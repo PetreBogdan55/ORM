3a265d81337e47496f6bf1f0a7a229e8a3e19111
Petre Bogdan <petrebogdan55@yahoo.com>
2023-06-17 00:57:06 +0300

    Update conn string

--- TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
+++ TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionHelper {
-    private static final String url = "jdbc:postgresql://34.77.61.51:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=unified-firefly-389410:europe-west1:postgres&user=postgres&password=postgres";
+    private static final String url = "jdbc:postgresql://34.17.41.6:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=unified-firefly-389410:europe-west12:postgres&user=postgres&password=postgres";
 
     public static Connection getConnection() {
         Connection connection = null;
