687944100073fce59287da38acd1cf670424c28a
Petre Bogdan <petrebogdan55@yahoo.com>
2023-06-19 19:04:04 +0300

    ThirdPartyAPI

--- TPD_Server/src/main/java/com/example/tpd_server/controllers/GoogleBooksController.java
+++ TPD_Server/src/main/java/com/example/tpd_server/controllers/GoogleBooksController.java
@@ -3,49 +3,27 @@ package com.example.tpd_server.controllers;
 import com.example.tpd_server.models.GoogleBook;
 import jakarta.ws.rs.*;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.io.BufferedReader;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/google-books")
 public class GoogleBooksController {
+    private static final String API_URL = "https://www.googleapis.com/books/v1/volumes?q=sun";
+
     @GET
     @Produces("application/json")
-    public ArrayList<GoogleBook> getAll() {
-        // Send GET request to Google Books API to fetch all books
-        String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-        String query = ""; // Customize the query based on your requirements
-        ArrayList<GoogleBook> books = new ArrayList<>();
+    public String getAll() {
+        Client client = ClientBuilder.newClient();
+        Response response = client.target(API_URL)
+                .request(MediaType.APPLICATION_JSON)
+                .get();
 
-        try {
-            URL url = new URL(apiUrl + query);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                // Parse the JSON response and populate the books ArrayList
-                // Implement the logic to extract relevant book information from the response
-
-            } else {
-                // Handle the error response
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+            return response.readEntity(String.class);
+        } else {
+            return "Error sending GET request. Response code: " + response.getStatus();
         }
-
-        return books;
     }
 }
